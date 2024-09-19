b5dd7c108352d71a79b12bb5481279893cb98a0b
Marcos Vinicius <144844828+v1nizx@users.noreply.github.com>
2024-09-19 00:06:09 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const sections =  document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+const themeBtn = document.querySelector('.theme-btn');
+
+function PageTransitions(){
+    //Button click active class
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+    /*document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })*/
+}
+PageTransitions();
